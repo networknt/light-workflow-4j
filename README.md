6a7869db09fcc842f759f77a47dcdb9440e325a0
Steve Hu <stevehu@gmail.com>
2017-10-03 19:58:45 -0400

    fixes #1 amazon states language implementation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # light-workflow-4j
-A state machine based light-weight workflow engine for microservices orchrestration
+A state machine based light-weight workflow engine for microservices orchestration.
+
+## States Language
+
+The states language is [Amazon States Language](https://states-language.net/spec.html) so a
+lot of developers should be familiar with it. 
+
+## State Engine
+
+State Engine is implemented in Java.
+
+ 
---
+++ state-builder/src/test/java/com/networknt/workflow/state/builder/internal/validation/PathValidationTest.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.networknt.workflow.state.builder.internal.validation;
+
+import static com.networknt.workflow.state.builder.StepFunctionBuilder.eq;
+
+import com.networknt.workflow.state.builder.StepFunctionBuilder;
+import org.junit.Test;
+import com.networknt.workflow.state.builder.states.Catcher;
+import com.networknt.workflow.state.builder.states.ParallelState;
+import com.networknt.workflow.state.builder.states.PassState;
+import com.networknt.workflow.state.builder.states.TaskState;
+import com.networknt.workflow.state.builder.states.WaitState;
+
+public class PathValidationTest {
+
+    @Test(expected = ValidationException.class)
+    public void choiceState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ChoiceState")
+                .state("ChoiceState", StepFunctionBuilder.choiceState()
+                        .choice(StepFunctionBuilder.choice()
+                                        .condition(StepFunctionBuilder.eq("$.foo", "bar"))
+                                        .transition(StepFunctionBuilder.next("End")))
+                        .inputPath("$."))
+                .state("End", StepFunctionBuilder.succeedState())
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void choiceState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ChoiceState")
+                .state("ChoiceState", StepFunctionBuilder.choiceState()
+                        .choice(StepFunctionBuilder.choice()
+                                        .condition(StepFunctionBuilder.eq("$.foo", "bar"))
+                                        .transition(StepFunctionBuilder.next("End")))
+                        .outputPath("["))
+                .state("End", StepFunctionBuilder.succeedState())
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void choiceState_ConditionWithInvalidPath_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ChoiceState")
+                .state("ChoiceState", StepFunctionBuilder.choiceState()
+                        .choice(StepFunctionBuilder.choice()
+                                        .condition(StepFunctionBuilder.eq("$.", "bar"))
+                                        .transition(StepFunctionBuilder.next("End"))))
+                .state("End", StepFunctionBuilder.succeedState())
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void parallelState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ParallelState")
+                .state("ParallelState", validParallelState().inputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void parallelState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ParallelState")
+                .state("ParallelState", validParallelState().outputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void parallelState_ResultPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ParallelState")
+                .state("ParallelState", validParallelState().resultPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void parallelState_ResultPathInvalidReferencePath_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("ParallelState")
+                .state("ParallelState", validParallelState().resultPath("$.foo[*]"))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void passState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("PassState")
+                .state("PassState", validPassState().inputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void passState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("PassState")
+                .state("PassState", validPassState().outputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void passState_ResultPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("PassState")
+                .state("PassState", validPassState().resultPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void passState_ResultPathInvalidReferencePath_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("PassState")
+                .state("PassState", validPassState().resultPath("$.foo[*]"))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void succeedState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("SucceedState")
+                .state("SucceedState", StepFunctionBuilder.succeedState().inputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void succeedState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("SucceedState")
+                .state("SucceedState", StepFunctionBuilder.succeedState().outputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().inputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().outputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_ResultPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().resultPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_ResultPathInvalidReferencePath_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().resultPath("$.foo[*]"))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_CatcherWithResultPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().catcher(validCatcher("End").resultPath("$.")))
+                .state("End", StepFunctionBuilder.succeedState())
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void taskState_CatcherResultPathInvalidReferencePath_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("TaskState")
+                .state("TaskState", validTaskState().catcher(validCatcher("End").resultPath("$.foo[*]")))
+                .state("End", StepFunctionBuilder.succeedState())
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void waitState_InputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("WaitState")
+                .state("WaitState", validWaitState().inputPath("$."))
+                .build();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void waitState_OutputPathInvalid_ThrowsException() {
+        StepFunctionBuilder.stateMachine()
+                .startAt("WaitState")
+                .state("WaitState", validWaitState().inputPath("$.foo[*}"))
+                .build();
+    }
+
+    private ParallelState.Builder validParallelState() {
+        return StepFunctionBuilder.parallelState()
+                .comment("My parallel state")
+                .inputPath("$.input")
+                .outputPath("$.output")
+                .resultPath("$.result")
+                .transition(StepFunctionBuilder.end())
+                .branches(
+                        StepFunctionBuilder.branch()
+                                .comment("Branch one")
+                                .startAt("BranchOneInitial")
+                                .state("BranchOneInitial", StepFunctionBuilder.succeedState()),
+                        StepFunctionBuilder.branch()
+                                .comment("Branch two")
+                                .startAt("BranchTwoInitial")
+                                .state("BranchTwoInitial", StepFunctionBuilder.succeedState())
+                         );
+    }
+
+    private WaitState.Builder validWaitState() {
+        return StepFunctionBuilder.waitState()
+                .waitFor(StepFunctionBuilder.seconds(10))
+                .transition(StepFunctionBuilder.end());
+    }
+
+    private PassState.Builder validPassState() {
+        return StepFunctionBuilder.passState().transition(StepFunctionBuilder.end());
+    }
+
+    private TaskState.Builder validTaskState() {
+        return StepFunctionBuilder.taskState()
+                .resource("foo")
+                .transition(StepFunctionBuilder.end());
+    }
+
+    private Catcher.Builder validCatcher(String catcherTransition) {
+        return StepFunctionBuilder.catcher().transition(StepFunctionBuilder.next(catcherTransition)).catchAll();
+    }
+}
